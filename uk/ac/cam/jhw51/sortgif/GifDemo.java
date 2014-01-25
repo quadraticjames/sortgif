818c5ae04382adadb208a43f3d6427381a776cea
quadraticjames <jwizzle.uk@gmail.com>
2014-01-25 12:59:34 +0000

    Add insertion sort

--- uk/ac/cam/jhw51/sortgif/GifDemo.java
+++ uk/ac/cam/jhw51/sortgif/GifDemo.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class GifDemo {
 	public static void main(String[] args) throws IOException {
-		//args[0] = number of integers in array to be sorted; args[1] = output filename
+		//args[0] = number of integers in array to be sorted; args[1] = output directory
 		List<Integer> l = new LinkedList<Integer>();
 		for (int i = 0; i < Integer.parseInt(args[0]); i++) { 
 			l.add(i+5);
@@ -18,7 +18,12 @@ public class GifDemo {
 		for (int j = 0; j < l.size(); j++) {
 			i[j] = l.get(j);
 		}
-		Sorter s = new HeapSorter(i,new FrameBuilder(new OutputGif(args[1])));
+		Sorter s = new HeapSorter(i.clone(),new FrameBuilder(new OutputGif(args[1] + "heap.gif")));
 		s.sort();
+		s = new QuickSorter(i.clone(),new FrameBuilder(new OutputGif(args[1] + "quick.gif")));
+		s.sort();
+		s = new InsertionSorter(i.clone(),new FrameBuilder(new OutputGif(args[1] + "insert.gif")));
+		s.sort();
+		
 	}
 }
