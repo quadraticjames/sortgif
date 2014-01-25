328cc5b5e2661f7f5f73d01eb13e78745cfbc3a9
quadraticjames <jwizzle.uk@gmail.com>
2014-01-25 17:28:17 +0000

    Implement bubblesort

--- uk/ac/cam/jhw51/sortgif/BubbleSorter.java
+++ uk/ac/cam/jhw51/sortgif/BubbleSorter.java
@@ -9,7 +9,6 @@ public class BubbleSorter extends Sorter {
 	public BubbleSorter(int[] is, FrameBuilder fb) {
 		this.a = is;
 		this.mFB = fb;
-		
 	}
 
 	@Override
@@ -19,7 +18,18 @@ public class BubbleSorter extends Sorter {
 	}
 	
 	private void bubbleSort() {
-		
+		boolean didSwaps = true;
+		int passes = 0;
+		while (didSwaps) {
+			//ASSERT: [a.length - (1 + passes):a.length] is sorted
+			didSwaps = false;
+			for (int k = 0; k < a.length - (1 + passes); k++) {
+				if (a[k] > a[k+1]) {
+					int tmp = a[k]; a[k] = a[k+1]; a[k+1] = tmp;
+					didSwaps = true;
+				}
+			}
+		}
 	}
 
 }
